0ffba0be1fa893e89ccd90952466bfc9c7425e55
OferRavid <ofer.rvd@gmail.com>
2025-06-15 01:45:32 +0300

    Handling command line arguments

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    if len(sys.argv) <= 1:
+        print("Usage: python main.py <user_prompt> [--verbose]")
+        exit(1)
+    user_prompt = sys.argv[1]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=user_prompt,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
